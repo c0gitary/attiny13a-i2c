ecbd39ade9cc9a898f332b5bd5933a273db2ef44
c0gitary <84321459+c0gitary@users.noreply.github.com>
2025-06-18 22:16:34 +0400

    fix

--- tn13a_i2c.h
+++ tn13a_i2c.h
@@ -3,9 +3,9 @@
 
 typedef unsigned char uint8_t;
 
-extern void avr_i2c_init(void);
-extern void avr_i2c_stop(void);
-extern void avr_i2c_start(uint8_t addr);
-extern void avr_i2c_write(uint8_t data);
+extern void tn13a_i2c_init(void);
+extern void tn13a_i2c_stop(void);
+extern void tn13a_i2c_start(uint8_t addr);
+extern void tn13a_i2c_write(uint8_t data);
 
 #endif //__ATTINY13A_SOFT_I2C__
